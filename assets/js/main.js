5ce6855bbf5ae119a9e44d201b2bbb3a61be15b2
adel <nadl61802@gmail.com>
2026-03-26 12:59:49 +0200

    Add new service image and enhance navigation functionality
    - Added a new service image (service-img-3.webp) to the assets.
    - Improved navigation menu functionality with show/hide feature on toggle click.
    - Implemented link action to remove the menu on mobile when a link is clicked.
    - Added shadow effect to the header on scroll.
    - Updated favicon to use a WebP format.
    - Enhanced accessibility by adding titles and aria-labels to navigation links.
    - Expanded the home section with social media links and contact information.
    - Introduced service section with descriptive cards for ordering process, delivery speed, and quality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,33 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (navId, toggleId) => {
+    const nav = document.getElementById(navId),
+        toggle = document.getElementById(toggleId)
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu')
+    })
 
-
+}
+showMenu('nav-menu', 'nav-toggle')
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
-
-
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 /*=============== SWIPER REVIEWS ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
